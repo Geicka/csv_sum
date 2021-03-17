f1f08ab3ea3cb4155e29f7cad43654ac7d70ce5a
Gustavo Fonseca <xxgusbrasilxx@gmail.com>
2021-03-16 17:36:17 -0700

    Update csv_sum.py

--- csv_sum.py
+++ csv_sum.py
@@ -5,11 +5,13 @@ from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
 from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
 from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
 
-
+# given path loads data as dataframe
 def loadDataAsDataFrame(f_path):
     df = pd.read_csv(f_path, encoding="ISO-8859-1")
+    # encoding allows system to properly consume special characters
     return df
 
+# saves all headers from dataframe in a list
 def header():
     title = data['title']
     score = data['score']
@@ -22,6 +24,7 @@ def header():
     h = [title, score, ids, url, comms_num, created, body, timestamp]
     return h
 
+# gtech summarize function
 def NLP_summarize(description: str):
 
     # Object of automatic summarization.
@@ -34,10 +37,10 @@ def NLP_summarize(description: str):
     abstractable_doc = TopNRankAbstractor()
     # Summarize document.
     result_dict = auto_abstractor.summarize(description, abstractable_doc)
-    
-    # print(result_dict)
-    # print()
-
+    '''commented out print statement in gtech's original function
+    print(result_dict)
+    print()
+    '''
     # Output result. Find sentence with the maximum score value
     maxi = [0, 0]
     for result in result_dict["scoring_data"]:
@@ -49,16 +52,24 @@ def NLP_summarize(description: str):
     else:
         return None
 
-
+# file path
 f_path = './reddit_wsb.csv'
+# calls loadDateAsDataFrame function as data
 data = loadDataAsDataFrame(f_path)
+# calls header function as head
 head = header()
-title = head[0]
+# calls body header from the header function
 body = head[6]
-
-blah = []
+# initialize new list for summaries
+summarizedList = []
+# loops through the items in body list
 for line in body:
+    # checks if the items being looped through exist
     if pd.notna(line):
+        # checks the number of sentences of the currenct item in loop as num_sent
         num_sent = len(st(line))
+        # checks to see if there is more than one sentence in the 
+        # current item of loop to selectively summary and append to new list
         if num_sent > 1:
-            blah.append(NLP_summarize(line))
+            summarizedList.append(NLP_summarize(line))
+            
